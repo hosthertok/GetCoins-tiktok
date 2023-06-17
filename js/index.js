9b487be6e65adfb14423a90a0bfed13fb38af37b
nxg bd <nxg@gmai.com>
2023-06-17 21:33:46 +0700

    2

---
+++ js/index.js
@@ -0,0 +1,106 @@
+var submit = document.getElementById("btn-submit");
+var inputCustum = document.getElementById("input-coin");
+var back = document.getElementById("btn-back");
+var btnLogin = document.getElementById("header-login-button");
+var btnLogout = document.getElementById("header-logout-button");
+
+for (let i = 1; i < 9; i++) {
+    document.getElementById("btn-coin" + i).addEventListener("click", function () {
+        if (
+            document.getElementById("btn-coin" + i).classList.contains("tiktok-l4v9n8-ButtonContainer")
+        ) {
+            document.getElementById("btn-coin" + i).classList.remove("tiktok-l4v9n8-ButtonContainer");
+            document.getElementById("btn-coin" + i).classList.add("tiktok-1xsxoj2-ButtonContainer");
+        }
+        for (let j = 1; j < 9; j++) {
+            if (j != i) {
+                document.getElementById("btn-coin" + j).classList.remove("tiktok-1xsxoj2-ButtonContainer");
+                document.getElementById("btn-coin" + j).classList.add("tiktok-l4v9n8-ButtonContainer");
+            }
+        }
+
+        // khi đến phần tử cuối thì khi lick vào sẽ ẩn phần tử con có id = custun-coin và hiện phần tử con khác lên có id = input-coin và khi click và chỗ khác thì nó về lại như ban đầu
+        document.getElementById("custun-coin").style.display = "block";
+        document.getElementById("input-coin").style.display = "none";
+
+        if (i == 8) {
+            document.getElementById("custun-coin").style.display = "none";
+            document.getElementById("input-coin").style.display = "block";
+            var inputCustom = document.getElementById("input-custom");
+            inputCustum.addEventListener("input", function () {
+                var inputValue = inputCustum.value;
+                if (isNaN(inputValue)) {
+                    alert("Vui lòng chỉ nhập số!");
+                    return;
+                }
+                var result = parseInt(inputValue) * 274;
+                var total = result / 23500;
+                document.getElementById("total-money").innerHTML = "$" + total.toFixed(2);
+                document.getElementById("money").innerHTML = "$" + total.toFixed(2);
+                document.getElementById("text-coin-result").innerHTML = inputValue;
+                if (inputValue == "") {
+                    document.getElementById("total-money").innerHTML = 0;
+                    document.getElementById("money").innerHTML = ' Large amount supported';
+                }
+
+            });
+
+        }
+
+        let money = document.querySelector("#btn-coin" + i + " .tiktok-10102vh-SpanTextNow").innerHTML;
+        document.getElementById("total-money").innerHTML = money;
+        let coin = document.querySelector("#btn-coin" + i + " .tiktok-wkgdjt-SpanNumDisplay").innerHTML;
+        document.getElementById("text-coin-result").innerHTML = coin;
+
+    })
+
+}
+
+submit.addEventListener("click", function () {
+    document.getElementById("resultload").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("resultload").style.display = "none";
+        document.getElementById("result").style.display = "block"
+    }, 2000);
+    
+    document.getElementById("input-coin").value = "";
+})
+
+back.addEventListener("click", function () {
+    document.getElementById("result").style.display = "none";
+    document.getElementById("custun-coin").style.display = "block";
+    document.getElementById("input-coin").style.display = "none";
+    document.getElementById("money").innerHTML = ' Large amount supported';
+    document.getElementById("total-money").innerHTML = "$";
+})
+
+
+btnLogin.addEventListener("click", function () {
+    document.getElementById("resultload").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("resultload").style.display = "none";
+        document.getElementById("infor").classList.toggle("show");
+        btnLogin.classList.add('unshow');
+        document.getElementById("infor-poin").classList.add('show');
+        document.getElementById("login-text").classList.add('unshow');
+        submit.removeAttribute("disabled")
+        document.getElementById("receiver").style.display = "block";
+    }, 2000);
+})
+
+btnLogout.addEventListener("click", function () {
+    document.getElementById("resultload").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("resultload").style.display = "none";
+        document.getElementById("infor").classList.toggle("show");
+        btnLogin.classList.remove('unshow');
+        document.getElementById("infor-poin").classList.remove('show');
+        document.getElementById("login-text").classList.remove('unshow');
+        submit.setAttribute("disabled", "disabled")
+        document.getElementById("receiver").style.display = "none";
+    }, 2000);
+})
+
+
+
+
